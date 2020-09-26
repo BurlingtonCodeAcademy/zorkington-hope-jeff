7b8a2d6e6ef05e7cd533149359310310c05790bb
hopeswanke <hopeswanke@gmail.com>
2020-09-26 16:49:33 -0400

    progress made 9/26

--- index.js
+++ index.js
@@ -7,51 +7,66 @@ function ask(questionText) {
     readlineInterface.question(questionText, resolve);
   });
 }
-// ------------------------------------------ player -------------------------------------------------
+// --------------------------------------- player object---------------------------------
 const player = {
   location: 'hangar',
   status: 'tired',
-  inventory: [],                    // do we want to add in stamina? 
+  inventory: [],
+  changeStatus: function () {
+    let newStatus = 'fully loaded'
+    this.status = newStatus
+  }
 }
 
-//-------------------------------------------- descriptions ------------------------------------------
-//room descriptions
+
+//-------------------------------- room generator ------------------------------------
+//room class with descriptions
 class Room {
-  constructor(name, desc, inv, locked) {
+  constructor(name, description, [inv], locked, power) {
     this.name = name,
-      this.desc = desc,
-      this.inv = inv,
+      this.desc = description,
+      this.inv = [inv],
       this.locked = locked
+      this.power = power
   }
+  changePower(){
+    this.power = true
+  }
+  changeLock() {
+    this.locked = false
+  }
+
   moveBetweenRooms(move) {
     moveToNewRoom(move)
   }
 }
-const hangar = new Room('hangar', 'You are now in the hangar. ', ['inv'], false)         //can we create an inventory that is randomly generated?? Ask Bob
-const engineBlock = new Room('Engine Block', 'You are now in the engine block', ['inv'], true)
-const hangarControlRoom = new Room('Hangar Control Room', 'you are now in the hangar control room', ['inv'], true)
-const officerQuarters = new Room('Officer Quaters', 'you are now in the officer quarters', ['inv'], true)
-const messHall = new Room('Mess Hall', 'you are now in the mess hall', ['inv'], false)
-const generatorRoom = new Room('Generator Room', 'you are now in the generator room', ['inv'], false)
+const hangar = new Room('hangar', 'You are now in the Hangar.\n\nYou find yourself in a dimly-lit room. The air around you is cold and stale. There is a workbench nearby with tools, anti-static equipment and a blueprint. There are three doors leading out.\n ', ['Blueprint'], false, true) 
+const engineBlock = new Room('Engine Block', 'You are now in the Engine Block ', ['ECM'], true, true)
+const hangarControlRoom = new Room('Hangar Control Room', 'You find yourself standing before the hangar control room door. ', ['coveralls'], true, false)
+const officerQuarters = new Room('Officer Quarters', 'You are now in the officer quarters.\nAround the room, you see bunked beds, chairs, and writing tables. On the wall above a writing table is a bulletin board full of schedules, duty rosters ... and a random notepad with something scrawled upon it. ', ['notepad'], true, true)
+const messHall = new Room('Mess Hall', 'You are now in the mess hall.\nIt appears that the last meal listed on the menu board was Taco Suprise. You are not feeling particularly hungry but thirst is gnawing at you. ', ['energyDrink', 'Coffee Cup'], false, true)
+const generatorRoom = new Room('Generator Room', 'You are now in the generator room.\n As you move through the archway into this room, you notice various consoles full of buttons and gauges, shelves with parts bins stacked up, as well as a pile of spare parts, including a fuse, laid out upon a small workbench. ', ['fuse'], false, true)
 
-//item descriptions
+//item class with descriptions
 class Item {
-  constructor(name, desc, takeable) {
+  constructor(name, description, takeable) {
     this.name = name,
-      this.desc = desc,
+      this.desc = description,
       this.takeable = takeable
   }
 
 }
-const energyDrink = new Item('Energy Drink', 'this is an energy drink', true)
-const ECM = new Item('ECM', 'this is an ECM', true)
-const coffeeCup = new Item('Coffee Cup', 'this is a coffee cup', false)
-const journal = new Item('Journal', 'this is a journal', false)
-
-
-
-// --------------------------------------transitions ------------------------------------
-
+const energyDrink = new Item('Energy Drink', 'That energy drink sure looks refreshing.', true)
+const coveralls = new Item('coveralls', 'Coveralls are worn by the enlisted staff.', false)
+const ECM = new Item('ECM', 'The ECM is necessary to switch fuels for a quantum jump.', true)
+const coffeeCup = new Item('Coffee Cup', 'Caffeine makes the days fly by.', false)
+const journal = new Item('Journal', 'This is a daily record kept by officers.', false)
+const notepad = new Item('notepad', 'I should probably remember this: 1223.', true)
+const blueprint = new Item('Blueprint', 'At the bottom of the blueprint, it reads: thanks for all the fish.', true)
+const key = new Item('Officer Quarters key', 'This key grants access to the Officer Quarters.', true)
+const fuse = new Item('Fuse', "generic fuse, probably useful somehow", true)
+// -------------------------------- transition rules ------------------------------------
+//allowable room transitions
 let shipRooms = {
   hangar: { canChangeTo: ['engineBlock', 'hangarControlRoom', 'messHall'] },
   hangarControlRoom: { canChangeTo: ['hangar', 'officerQuarters'] },
@@ -63,7 +78,7 @@ let shipRooms = {
 
 //--------------------------------- Lookup Tables --------------------------
 
-//Look up table for movements
+//Lookup table for allowable movement or action by the player
 let playerMovements = {
   move: ['west', 'east', 'north', 'south'],
   drink: ['take a drink', 'drink'],
@@ -71,7 +86,7 @@ let playerMovements = {
   leave: ['leave', 'drop'],
 }
 
-//Look up table for rooms
+//Lookup table for room names
 let roomsLookUp = {
   'hangarControlRoom': hangarControlRoom,
   'engineBlock': engineBlock,
@@ -81,79 +96,172 @@ let roomsLookUp = {
   'generatorRoom': generatorRoom,
 }
 
-//Look up table for items
+//Lookup table for inventory items
 let itemLookUp = {
-  'energyDrink': energyDrink,
+  'Energy Drink': energyDrink,
+  "coveralls": coveralls,
   'ECM': ECM,
-  'coffee cup': coffeeCup,
-  'journal': journal,                                    // do we add another possible way of finding engineBlock here?
-
+  'Coffee Cup': coffeeCup,
+  'Journal': journal,
+  'notepad': notepad,
+  'Blueprint': blueprint,    
+  'key': key,
+  "fuse": fuse
 }
 
-//-------------------------------------- movement from rooms -----------------------------------------
+//------------------------------ movement to and from rooms -------------------------------
 
 async function moveToNewRoom(newRoom) {
   let changeRoom = shipRooms[player.location].canChangeTo;
   if (changeRoom.includes(newRoom)) {
-    player.location = newRoom                                            // add in locked doors later
+    if (player.inventory.includes['fuse']){
+      hangarControlRoom['changePower']()
+    } else{
+      console.log('')}
+    if (player.inventory.includes('key')){
+      officerQuarters['changeLock']()
+    }
+    if (roomsLookUp[newRoom].locked) {
+      console.log('This room is locked. ')
+    } else {
+      player.location = newRoom
+    }                                         
   } else {
-    console.log('you cant go that way :/ ')
+    console.log('You cant go that way! ')
   }
 }
 
+//unlocking the engine block room
+async function passcodeEngineBlock(){
+  let passcodeQ = await ask('What is the Passcode ')
+  if (passcodeQ === '1223'){
+    engineBlock.locked = false
+    console.log('Access Granted ')
+  } else{
+    console.log('incorrect passcode ')
+  }
+}
 
+// async function turnPowerOnInHangarControlRoom(){
+//   if (hangarControlRoom.power === true){
+//     console.log('Access Granted ')
+//   } else {
+//     console.log('The door\'s control panel seems fried; however, it seems like an easy fix... You\'ll just need to replace the fuse.')
+//   }
 
+// }
+// --------------------------- The game story code is below this line ---------------------------
 launch();
 
 async function launch() {
-  const welcomeMessage = `You don’t know how you survived your last encounter with space pirates\nBut it seems luck was on your side\n...well not entirely\nYou’re ECM (electronic control module) was damaged in your last emergency jump drive.\nWithout the ECM you’re unable to switch fuel types, making another jump out the Virgo system, back to Orin, your home system, impossible.\n\nAre you ready?`;
+  const welcomeMessage = `You don’t know how you survived your last encounter with space pirates.\nBut it seems luck was on your side\n...Well not entirely.\nYour ECM (electronic control module) was damaged in your last emergency jump drive.\nWithout the ECM you’re unable to switch fuel types, making another quantum jump out the Virgo system, back to Orin, your home system, impossible.\n\nAre you ready? `;
   let answer = await ask(welcomeMessage);
   if (answer !== 'yes') {
-    console.log('Well you die then')
+    console.log('okay you die then')
     process.exit()
   } else {
-    console.log('Alert: Your hydrogen fuel levels are critical, immediate refueling required!!!!');
+    console.log('Alert: Your hydrogen fuel levels are critical. Immediate refueling required!!!');
   }
-
-  let hydroResponse = await ask('What are you going to do?\nyou can check fuel by pulling lever\nDo you pull lever\nyes or no? ')
-  // ------------------remember to san-----------------
+  let hydroResponse = await ask('What are you going to do?\nyou can check fuel by pulling the lever.\nDo you pull the lever\nyes or no? ')
+  // ------------------remember to sanitize, be descriptive with variable names-----------------
   if (hydroResponse !== 'yes') {
-    console.log('Well you die then')
+    console.log('By not pulling the lever your fuel runs out and you die...blissfully ignorant')
     process.exit()
   } else {
-    console.log(`\nYou estimate roughly 4 hours of flight time left with your remaining hydrogen fuel.\nyou will need to find a way to fix your ECM to make a Quantum jump \nor...you will be stuck floating in this system forever.\n\nBy stroke of pure luck your radar picks up a larger vessel relatively close by.`)
+    console.log(`\nYou estimate roughly 4 hours of flight time left with your remaining hydrogen fuel.\nYou will need to find a replacement ECM to make a Quantum jump or ... you will be stuck floating in this system forever.\n\nBy stroke of pure luck your radar picks up a much larger vessel relatively close by.`)
   }
-
   let nextStep = await ask('Should you land on the ship?\nyes or no? ')
   if (nextStep !== 'yes') {
-    console.log('Well you die then')
+    console.log('Well...you have succumbed to the vast, cold emptiness of space.')
     process.exit()
   } else {
-    console.log('you land on the ship\'s landing pad')
+    console.log('You touch down on the ship\'s landing pad.')
   }
-  console.log('You notice a light flickering and as you look around you notice the ship has lost power\nthe ship is sinking closer to a black hole\nYou need to find this ships ECM to fix your ship before the black hole consumes you and the ship\nyou find a blueprint of the ship\nthe blueprint says the ship\'s spare parts are in the Engine Block')
+  console.log('You notice a light flickering above the dimly lit hangar, and as you continue your initial obeservations, it becomes quite apparent that the ship has lost power.\nThe ship is sinking closer to a black hole!\nYou need to find this ship\'s replacement ECM to repair your ship before the black hole consumes you and the ship.\n\n\nYou can change rooms by typing "move"\nYou can take an item for your inventory by typing "take".\nYou can drop an item by typing "drop".\nTo check your inventory type "check inventory"\nTo check your status type "status".\nTo read an item type "read"\n\n')
+
+  let blueprintQ = await ask('You find a blueprint of the ship.\nThe blueprint says the ship\'s spare parts are in the Engine Block.\n do you take or leave. ')
+  if (blueprintQ === 'leave'){
+    console.log('you put the blueprint back down')
+  } else{
+    console.log('Oh no, the poison left on the blueprint spreads throughout your body and you have now died')
+    process.exit()
+  }
+
+
   console.log((roomsLookUp[player.location].desc))
+  //---------------- Main Story code: move, take, drop, status, inventory ----------
   while (true) {
     let room = roomsLookUp[player.location];
-    let question = await ask('What do you want to do? ')
+    let question = await ask('move, drink, take, drop, check inventory, read, status\nWhat do you want to do? ')
     if (question === 'move') {
       console.log(shipRooms[player.location].canChangeTo)
-      let clarifyingQ = await ask('What room do you want to go to ')
+      let clarifyingQ = await ask('What room do you want to go to? ')
+      if (clarifyingQ === 'engineBlock' && engineBlock.locked === true){
+        await passcodeEngineBlock()
+      }
+      // if (clarifyingQ === 'hangarControlRoom'){
+      //   console.log('lol')
+      //   turnPowerOnInHangarControlRoom()       // this room wont unlock even if the fuse is in inventory... aslo make sure the officerQuarters will unlock with key in inventory 
+        
+      // }
       room.moveBetweenRooms(clarifyingQ)
       room = roomsLookUp[player.location]
       console.log(room.desc)
     }
     else if (question === 'take') {
-      let takeQ = await ask('what would you like to take? ')
+      let takeQ = await ask('What would you like to take? ')
       if (room.inv.includes(takeQ)) {
-        console.log('You have taken this item')
+        console.log('You have taken this item for your inventory.')
         player.inventory.push(takeQ)
       } else {
-        console.log('That item is not in this room ')
+        console.log('That specific item is not found in this room. Look elsewhere. ')
+      }
+    }
+    else if (question === 'drop') {
+      let dropQ = await ask('What would you like to drop? ')
+      if (player.inventory.includes(dropQ)) {
+        console.log('You have dropped this item. ')
+        player.inventory.pop[dropQ]
+      } else {
+        console.log('That specific item is not found in your inventory. ')
+      }
+    }
+    else if (question === 'check inventory') {
+      let inventoryQ = await ask('Would you like to check your inventory? ')
+      if (inventoryQ === 'yes') {
+        console.log('The items you have in your inventory are: ' + player.inventory)
+      } else {
+        console.log('OK ... moving on ')
+      }
+    }
+    else if (question === 'status') {
+      let statusQ = await ask('Would you like to check your status? ')
+      if (statusQ === 'yes') {
+        console.log('You are currently feeling: ' + player.status)
+      } else {
+        console.log('OK ... moving on ')
+      }
+    }
+    else if (question === 'drink') {
+      let drinkQ = await ask('Would you like to drink this? ')
+      if (drinkQ === 'yes') {
+        console.log(player.status)
+        player['changeStatus']()
+        console.log('You are currently feeling: ' + player.status)
+      } else {
+        console.log('OK ... moving on ')
+      }
+    }
+    else if (question === 'read'){
+      let readQ = await ask('What item in your inventory would you like to read? ')
+      if (readQ === 'notepad'){
+        console.log(notepad.desc)
       }
     }
   }
-}
+}         // need a victory condition else if as well 
+
+
 //We have made it to here and have logic that allows you to move between room and take items, and player status -ish
 
 //let powerOn = false           // we will worry about this later
@@ -227,6 +335,3 @@ async function launch() {
 
 
 
-
-// function enterState(newState){
-//   let validTransitions = houseRooms[currentRoom].canChangeto;
